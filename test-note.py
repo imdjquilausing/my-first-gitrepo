9caa9bd14d899906f6f2d1802906e433170b924e
Daniel Quilausing <imdjquilausing@gmail.com>
2024-10-04 14:22:18 +0800

    update for test note

--- test-note.py
+++ test-note.py
@@ -1,4 +1,4 @@
-stack = []
+sss = []
 
 print("Catch and eat any of these fruits: ('Apple', 'orange','mango','Guave')")
 x = int(input("Enter how many fruits you would like to eat :"))
@@ -8,23 +8,23 @@ print("Choose a fruit catch. Press A, O, M or G")
 for i in range(x):
   fruit = input(f"Fruit {i + 1} of {x} :" )  
   if fruit == 'A' or fruit == 'a':
-    stack.append("Apple")
+    sss.append("Apple")
   elif fruit == 'O' or fruit == 'o':
-    stack.append("orange")
+    sss.append("orange")
   elif fruit == 'M' or fruit == 'm':
-    stack.append("Mango")
+    sss.append("Mango")
   elif fruit == 'G' or fruit == 'g':
-    stack.append("Guava")
+    sss.append("Guava")
   else: 
     print("Invalid input")  
 
-print("Your basket now has:" ,stack)
+print("Your basket now has:" ,sss)
 
 for i in range(x):
   eat = input("Press e to eat the fruit: ")
-  stack.pop()
+  sss.pop()
 
-  if not stack:
+  if not sss:
     print("Stack is empty")  
   else:
-    print("Fruit(s) in the basket" , stack)        
+    print("Fruit(s) in the basket" , sss)        
